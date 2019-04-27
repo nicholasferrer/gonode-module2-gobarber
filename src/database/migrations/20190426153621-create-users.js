27bd88db62adf20352bb8d2d9376e8f993a9d153
Nicholas Ferrer <nicholasferrer@hotmail.com>
2019-04-27 14:31:20 -0300

    user password encryption correcting errors

--- src/database/migrations/20190426153621-create-users.js
+++ src/database/migrations/20190426153621-create-users.js
@@ -22,7 +22,7 @@ module.exports = {
         allowNull: false,
         type: Sequelize.STRING
       },
-      password: {
+      password_hash: {
         allowNull: false,
         type: Sequelize.STRING
       },
